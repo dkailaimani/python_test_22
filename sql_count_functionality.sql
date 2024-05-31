0453bd47387a84cb016b0efd813db6fbde225de3
dkailaimani <102199241+dkailaimani@users.noreply.github.com>
2024-05-31 15:59:05 -0500

    Create sql_count_functionality.sql

---
+++ sql_count_functionality.sql
@@ -0,0 +1,5 @@
+--Produces a count of employees grouped by their department
+SELECT d.department_name, COUNT(e.employee_id) AS employee_count
+FROM Employees e
+JOIN Departments d ON e.department_id = d.department_id
+GROUP BY d.department_name;
