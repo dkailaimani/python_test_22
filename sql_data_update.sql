51db984ced0ad27ec4b6c8d080571db1f8ca651f
dkailaimani <102199241+dkailaimani@users.noreply.github.com>
2024-05-31 15:53:52 -0500

    Update sql_data_update.sql

--- sql_data_update.sql
+++ sql_data_update.sql
@@ -1,4 +1,4 @@
---Update WorkoutSessions table with change in session_time for Jane Doe
+--Update WorkoutSessions table with change in session_time for Florence and The Machine
 UPDATE WorkoutSessions
 SET session_time = 'Evening'
-WHERE member_name = 'Jane Doe';
+WHERE member_name = 'Florence and The Machine';
